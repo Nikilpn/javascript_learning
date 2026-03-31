528bdd171626d7c6764ebb778cf6d5299fd1e5e2
Nikilpn <nikhilpnnick1234@gmail.com>
2026-03-31 09:34:16 +0530

    keywords,datatypes

---
+++ javascript/prgram2.js
@@ -0,0 +1,12 @@
+demo="javascript"
+console.log(demo);
+console.log(typeof(demo));
+
+demo2=34
+console.log(demo2);
+console.log(typeof(demo2));
+
+
+demo3=false
+console.log(demo3);
+console.log(typeof(demo3));
