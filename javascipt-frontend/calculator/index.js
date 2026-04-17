a5b3168dbb0ea558241e20e089153d630fa6d4f0
Nikilpn <nikhilpnnick1234@gmail.com>
2026-04-17 14:01:11 +0530

    calculator app

---
+++ javascipt-frontend/calculator/index.js
@@ -0,0 +1,27 @@
+function display(num){
+    result.value+=num
+    
+}
+
+function clearAll(){
+    result.value=""
+}
+
+function back(){
+
+    result.value = result.value.slice(0, -1);
+}
+
+function equalTo(){
+    try{
+    result.value=eval(result.value)
+    }catch(error){
+        result.value="Error"
+        setTimeout(() => {
+            result.value=""
+            
+        }, 2000);
+
+
+    }
+}
